c9ee5199d353d0a1fd7876c04ae07dd47d88ae4a
Laura <lpvera22@gmail.com>
2018-04-25 15:44:45 -0300

    adelantando ejercicios

--- study/venv/ListaDobleEncad.py
+++ study/venv/ListaDobleEncad.py
@@ -75,7 +75,118 @@ def ListasIntercaladas(lista,lista2):
 
 
 
-ee64564
+def ImprimirLista(lista):#O(n)
+
+    while(lista.inicio.next!=None):
+        print(lista.inicio.data)
+        lista.inicio =lista.inicio.next
+
+def DestruirLista(lista):#O(1)
+
+    lista.inicio=lista.fin=None
+
+#EJERCICIO 2
+
+
+def InsertarListaOrd(lista,x):#O(n)
+    if len(lista)>=0:
+        lista.append(x)
+        i=len(lista)
+        while(i<=0):
+            if lista[i]<lista[i-1]:
+                aux=lista[i]
+                lista[i]=lista[i-1]
+                lista[i-1]=aux
+                i=i-1
+            else: i=i-1
+
+
+    else:
+        lista.append(x)
+def UnionConjuntos(conjunto1,conjunto2):#O(n^2)
+
+
+    for i in conjunto1:
+        if i not in conjunto2:
+            conjunto2.append(i)
+
+    return conjunto2
+
+def Interseccion(conjunto1, conjunto2):#O(n^2)
+    conjuntoInterseccion=[]
+    for i in conjunto1:
+        for j in conjunto2:
+            if i==j:
+                conjuntoInterseccion.append(i)
+    return conjuntoInterseccion
+
+def ConjuntosIguales(conjunto1, conjunto2):
+    flag=True
+    for i in conjunto1:
+        for j in conjunto2:
+            if i!=j:
+                flag=False
+    if not flag:
+        return False
+    else:
+        return True
+
+#EJERCICIO  4
+class Fila:
+    def __init__(self):
+        self.fila=[]
+        self.fin = len(self.lista)
+
+    def EsVacia(self):
+        if self.fin < 0:
+            return True
+        else:
+            return False
+class Pila:
+    def __init__(self):
+        self.pila=[]
+        self.tope=len(self.pila)
+
+    def EsVacia(self):
+        if self.tope < 0:
+            return True
+        else:
+            return False
+
+def Empilar(P,x):
+    P.pila.append(x)
+
+def Desempilar(P):
+    return P.pila.pop()
+
+def Enfilar(F,x):
+    F.fila.append(x)
+
+def Desenfilar(F):
+    aux=F.fila[F.fin]
+    return F.fila[aux]
+
+def Invertir(F):
+    pila=Pila()
+    while(F.fin>=0):
+        i=Desenfilar(F.fila)
+        Empilar(pila,i)
+    while(pila.tope>=0)
+        i=Desempilar(pila)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
