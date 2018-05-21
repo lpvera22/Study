55b1439494f79990b3f0a56d243fbd155736b2e1
Laura Pérez Vera <lpvera22@gmail.com>
2018-05-20 21:12:13 -0300

    Agregando algunos comentarios

--- study/algoritmo_mochila_ej23.py
+++ study/algoritmo_mochila_ej23.py
@@ -19,14 +19,19 @@ c = 0
 
 
 def ks(items, weight):
+#create 2 matrices one to save the best sums and another one to keep what elements we have taken.
     mem = [[0 for j in xrange(weight + 1)]
            for i in xrange(len(items) + 1)]
 
     grab = [[0 for j in xrange(weight + 1)]
             for i in xrange(len(items) + 1)]
-
+#iterate over all items and weights greater than and equal to 1. And for each iteration I am solving
+# a mini-problem of the backpack that will help me to solve a bigger backpack problem.  the
+#rows indicate the item and that the columns indicate the capacity of the backpack. So row (0, i) and
+# (i, 0) are initially at 0 because I either do not have items or I do not have capacity in the backpack.
     for i, item in enumerate(items, start=1):
         for j in xrange(1, weight + 1):
+
             if item.weight <= j:
                 if item.value + mem[i][j - item.weight] >= mem[i - 1][j]:
                     mem[i][j] = item.value + mem[i][j - item.weight]
