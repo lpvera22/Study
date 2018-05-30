da72155e851b6bf842859cdb6f292b54b745f3a4
Laura Pérez Vera <lpvera22@gmail.com>
2018-05-30 14:56:50 -0300

    fixing exercise 12

--- Lista_Ejercicios_3/Ejercicio_12.py
+++ Lista_Ejercicios_3/Ejercicio_12.py
@@ -15,36 +15,49 @@ def localizacionX(matriz,n,x):
             print('Element not found')
     else:
 
-        if matriz[n/2][0]>x:
-            fin=n/2
-            inicio=0
-
-        else:
-            inicio=n/2
-            fin=n
-        flag=True
-        while (inicio<=fin and flag):
-            if matriz[inicio][0]<x and matriz[inicio][n]>x:
-                fila=inicio
-                flag=False
-
-            else:inicio=inicio+1
 
         inicio=0
-        fin=n
-        flag1=False
+        fin=n-1
 
-        while(inicio<=fin and flag1):
+        while (inicio<fin ):
+            medio=(fin+inicio)/2
+            if matriz[medio][0]<=x :
+                if matriz[medio][n-1]>=x:
+                    inicio=medio
+                    fin=medio
+
+                else:
+                    inicio=medio+1
+            else:
+                fin=medio-1
+
+        fila = inicio
+        inicio=0
+        fin=n-1
+        flag1=True
+
+        while(inicio<=fin ):
             if matriz[fila][(inicio + fin) / 2] == x:
-                flag1=True
+                flag1=False
                 return fila, (inicio + fin) / 2
 
             elif x > matriz[fila][(inicio + fin) / 2]:
-                inicio = (fin+inicio)/2
+                inicio = (fin+inicio)/2+1
             else:
-                fin=(inicio+fin)/2
-        if flag1!=True:
-            print('Element not found')
+                fin=(inicio+fin)/2-1
+        print('Element not found')
+if __name__ == "__main__":
+    n = int(input('n'))
+    m=[]
+    for i in range(n):
+        sec = raw_input('secuencia').split()
+        S = [int(i) for i in sec]
+        m.append(S)
+    print(m)
+
+    x = int(input('x'))
+    print(localizacionX(m,n,x))
+
 
 
 
