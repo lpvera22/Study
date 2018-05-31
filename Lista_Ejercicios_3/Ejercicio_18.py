7698695b0f71f60febb32940487174796382c3b8
Laura Pérez Vera <lpvera22@gmail.com>
2018-05-31 17:25:56 -0300

    EJERCICIO 18 HECHO y comentado

--- Lista_Ejercicios_3/Ejercicio_18.py
+++ Lista_Ejercicios_3/Ejercicio_18.py
@@ -2,36 +2,36 @@
 def convex_hull(points):
 
 
-    # Sort the points lexicographically (tuples are compared lexicographically).
-    # Remove duplicates to detect the case we have just one unique point.
+    # Ordena os pontos de menos a maior pelo exio x.
+
     points = sorted(set(points))
 
-    # Boring case: no points or a single point, possibly repeated multiple times.
+    # Base: qando tenha 0 o só um punto retorna a lista.
     if len(points) <= 1:
         return points
 
-    # 2D cross product of OA and OB vectors, i.e. z-component of their 3D cross product.
-    # Returns a positive value, if OAB makes a counter-clockwise turn,
-    # negative for clockwise turn, and zero if the points are collinear.
+
+    #cross retorna se os pontos O A B estão na orden de anti-horaria retornando um valor positivo se estam na
+    #  orden horaria retorna o valor negativo e 0 se os pontos son colineares
     def cross(o, a, b):
         return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
 
-    # Build lower hull
+    # Construyendo o casco convexo mais baixo
     lower = []
     for p in points:
         while len(lower) >= 2 and cross(lower[-2], lower[-1], p) <= 0:
             lower.pop()
         lower.append(p)
 
-    # Build upper hull
+    # Construyendo o casco convexo da acima
     upper = []
     for p in reversed(points):
         while len(upper) >= 2 and cross(upper[-2], upper[-1], p) <= 0:
             upper.pop()
         upper.append(p)
 
-    # Concatenation of the lower and upper hulls gives the convex hull.
-    # Last point of each list is omitted because it is repeated at the beginning of the other list.
+
+    #Juntando os dois cascos convexos
     return lower[:-1] + upper[:-1]
-points=[(5,6),(6,2),(7,4),(1,5),(2,1),(3,3)]
+points=[(2,1),(6,1),(4,1),(5,1)]
 print(convex_hull(points))
