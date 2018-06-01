c6061c93d65899441726f1507d97b2868b36f267
Laura Pérez Vera <lpvera22@gmail.com>
2018-06-01 16:06:24 -0300

    ejercicio #25 LISTO!!

--- Lista_Ejercicios_3/Ejercicio_25.py
+++ Lista_Ejercicios_3/Ejercicio_25.py
@@ -20,6 +20,7 @@ return mem[n][m];
 def D(A,B):
     n=len(A)
     m=len(B)
+
     mem=[[-1]*(m+1) for i in range(n+1)]
     for i in range(n+1):
         for j in range(m+1):
@@ -31,9 +32,36 @@ def D(A,B):
                 mem[i][j] = 1 + min(mem[i - 1][j], mem[i][j - 1], mem[i - 1][j - 1])
     return mem
 
+
+def D_b(A, B):
+    n = len(A)
+    m = len(B)
+
+
+    mem = [[-1] * (m + 1) for i in range(n + 1)]
+
+    for i in range(n + 1):
+        mem[i][0] = []
+    for j in range(m + 1):
+        mem[0][j] = []
+
+
+
+    for i in range(1,n + 1):
+        for j in range(1,m + 1):
+
+            if A[i - 1] == B[j - 1]:
+                mem[i][j] = mem[i - 1][j - 1][:]
+            else:
+                aux=min(mem[i - 1][j], mem[i][j - 1], mem[i - 1][j - 1],key=lambda p:len(p))[:]
+                aux.append(A[i - 1])
+                mem[i][j]=aux
+
+    return mem
+
 A='laura'
 B='pelau'
-mem=D(A,B)
+mem=D_b(A,B)
 for i in mem:
     print(i)
 
