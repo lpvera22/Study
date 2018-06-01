fff16feccba505d56f3065fa6352428fbbba10f3
Laura Pérez Vera <lpvera22@gmail.com>
2018-06-01 03:06:19 -0300

    ejercicio #25

---
+++ Lista_Ejercicios_3/Ejercicio_25.py
@@ -0,0 +1,15 @@
+# *-* encoding:UTF-8 *-*
+def D(t,p):
+    n=len(t)
+    m=len(p)
+    if n>m:
+        adicion=
+        n=m
+    elif m>n:
+        m=n
+
+
+
+
+        for i in range(n):
+            for j in range(m):
