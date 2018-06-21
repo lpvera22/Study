09e04e6ddfc5906d4f5293aefd3ad483c6534b3c
Laura Pérez Vera <lpvera22@gmail.com>
2018-06-20 22:44:00 -0300

    creando greedy_camino_menor_costo

---
+++ Lista#4 de Ejercicios/arbol_cubrimiento_minimo_Prim.py
@@ -0,0 +1,13 @@
+# *-* encoding:UTF-8 *-*
+class GrafoListaAd:
+    def __init__(self):
+        self.listaAdy = {}
+        self.numerovertices = 0
+
+
+
+
+def arvol_geradora_mínima(G):
+    #G graph to return
+
+
---
+++ Lista#4 de Ejercicios/greedy_camino_menor_costo.py
@@ -0,0 +1,7 @@
+# *-* encoding:UTF-8 *-*
+def Shortest_path(v,cost,n):
+    #v initial vertex
+    #cost, is the matrix that contains the costs between all the edges
+    #n is the number of vertexs
+
+
