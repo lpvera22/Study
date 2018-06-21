3395197abbac55892aa3f7457158cf924407e82e
Laura Pérez Vera <lpvera22@gmail.com>
2018-06-21 11:37:01 -0300

    arreglando greedy seleccion de conjunto de cajas

--- Lista#4 de Ejercicios/greedy_conjunto_cajas.py
+++ Lista#4 de Ejercicios/greedy_conjunto_cajas.py
@@ -6,8 +6,8 @@ def subconjunto_cajas(p,K,c,n):
     #n, number of boxes
     peso_max=K
     B=[0]*n# list that's is going to have the result (the boxes with the value of 1)
-    '''taking in acount that in this case the optmimal selection es to in each step take the box 
-     with major cost, because we need to maximize the cost, so that's why I sort the list of costs (c) first'''
+    #taking in acount that in this case the optmimal selection es to in each step take the box
+    # with major cost, because we need to maximize the cost, so that's why I sort the list of costs (c) first
     c.sort(reverse=True)
     i=0
 
@@ -18,10 +18,15 @@ def subconjunto_cajas(p,K,c,n):
     return B
 if __name__=='__main__':
 
-    p=5
-    K=10
-    c=[1,2,5,7,0,10]
-    n=6
+    p=20
+    K=100
+    c={}
+    c1=[50,40,50,10,40]
+    for i in range(n):
+        c[i]=c1[i]
+
+
+    n=5
 
 
     print(subconjunto_cajas(p,K,c,n))
