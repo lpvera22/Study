901be0673fdb3a47f7cdd56746b27c3a25db7e14
Laura Pérez Vera <lpvera22@gmail.com>
2018-06-21 21:59:54 -0300

    greedy conjunto de cajas arreglado

--- Lista#4 de Ejercicios/all_par_vertices_short_path.py
+++ Lista#4 de Ejercicios/all_par_vertices_short_path.py
@@ -3,13 +3,19 @@ def all_pairs_shortest_path(costo,n):
     #costo, adjacency matrix of the graph
     #n, amount of vertex
 
+    camino_vi_vj= [[None]*n for i in range(n)]# matrix that it's going to return the paths
+    for i in range(n):
+        for j in range(n):
+            if costo[i][j]!=float('inf'):
+                camino_vi_vj[i][j]=i
 
     for m in range(n):
         for x in range(n):
             for y in range(n):
                 if costo[x][m]+costo[m][y]<costo[x][y]:
                     costo[x][y]=costo[x][m]+costo[m][y]
-    return costo
+                    camino_vi_vj[x][y]=m
+    return camino_vi_vj
 if __name__=='__main__':
 
     n=5
@@ -18,4 +24,9 @@ if __name__=='__main__':
     cost=[[0,float('inf'),2,1,float('inf')],[float('inf'),0,float('inf'),float('inf'),2],[float('inf'),1,0,float('inf'),1],[float('inf'),3,float('inf'),0,float('inf')],[float('inf'),float('inf'),float('inf'),float('inf'),0]]
 
 
-    print(all_pairs_shortest_path(cost,n))
+    x=all_pairs_shortest_path(cost,n)
+
+    for i in x:
+        for j in i:
+            print j,
+        print ('\n')
