5078fedf7b087be6c5d83d70572798717b728f41
Laura Pérez Vera <lpvera22@gmail.com>
2018-06-21 21:31:46 -0300

    greedy conjunto de cajas arreglado

---
+++ Lista#4 de Ejercicios/MCT_grafo_orientado.py
@@ -0,0 +1,9 @@
+# *-* encoding:UTF-8 *-*
+def MCT_grafo_orientado(v,a, costo):
+# costo, cost of each aresta
+# n, amount of vertex
+#a,arestas of the graph
+
+
+
+
--- Lista#4 de Ejercicios/greedy_conjunto_cajas.py
+++ Lista#4 de Ejercicios/greedy_conjunto_cajas.py
@@ -5,22 +5,22 @@ def subconjunto_cajas(p,K,c,n):
     #c, diccionary of the cost of each box
     #n, number of boxes
     peso_max=K
-
     #taking in acount that in this case the optmimal selection es to in each step take the box
-    # with major cost, because we need to maximize the cost, so that's why I sort the list of costs (c) first
-    costo=sorted(c,reverse=True)
+    # with major cost, because we need to maximize the cost, so that's why I sort dictionary
+    costo=sorted(c,key=c.get,reverse=True)
+
     i=0
-    for i in costo:
-        B[i]=0 # list that's is going to have the result (the boxes with the value of 1)
+    aux=[]
     while (i<=n and p<=peso_max):
-        B[i]=1
+        aux.append((costo[i],c[i]))
+
         peso_max=peso_max-p
         i+=1
-    return B
+    return aux
 if __name__=='__main__':
     n = 5
-    p=50
-    K=100
+    p=2
+    K=5
     c={}
     c1=[50,40,50,10,40]
     for i in range(n):
